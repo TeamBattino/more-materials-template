ab8f802774872ba198c3723907dc7bcb71206a9c
Battino <battino46@gmail.com>
2024-04-19 15:32:06 +0200

    add Helpfull Comments

--- src/main/java/ch/noseryoung/MoreMaterials.java
+++ src/main/java/ch/noseryoung/MoreMaterials.java
@@ -9,9 +9,14 @@ public class MoreMaterials implements ModInitializer {
 	// This is a Logger
 	public static final Logger LOGGER = LoggerFactory.getLogger("more-materials");
 
+	// TODO: Create instance of Item
 	@Override
 	public void onInitialize() {
 		// Loads on Startup
 		LOGGER.info("Hello Fabric world!");
+
+		// TODO: Add Item to Item Registry
+
+		// TODO: Add Item to Creative mode Tab
 	}
 }
