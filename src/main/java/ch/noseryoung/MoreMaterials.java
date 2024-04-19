8825389dd4c6485b479b06981eaf253f953a56e4
Battino <battino46@gmail.com>
2024-04-19 15:29:44 +0200

    update comments

--- src/main/java/ch/noseryoung/MoreMaterials.java
+++ src/main/java/ch/noseryoung/MoreMaterials.java
@@ -6,17 +6,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MoreMaterials implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("more-materials");
+	// This is a Logger
+	public static final Logger LOGGER = LoggerFactory.getLogger("more-materials");
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
+		// Loads on Startup
 		LOGGER.info("Hello Fabric world!");
 	}
 }
